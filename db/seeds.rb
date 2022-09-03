f4e1b4e65109f516e93b50911ae805e1d614e3a0
Rickie Natividad <rickienatividad85@gmail.com>
2022-09-03 12:44:23 -0400

    adds seeds.rb Actor instance

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+actor = Actor.new(first_name: "Kurt", last_name: "Russell", known_for: "Big Trouble in Little China")
+
+actor.save
